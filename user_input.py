ab6b53707aa60e1d59e4125d8b0fee3f15f2fd45
Bolarinwa M. Akinde - Transaction Officer <bakinde@Accion.local>
2024-11-01 10:06:36 +0100

    My python assigment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+# Ask for user details
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
